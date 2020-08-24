b72fae15866e0fd03ae5dae44da0ea7915c8946b
王立广 <wangliguang@kaikeba.com>
2020-08-24 16:41:11 +0800

    ✨ RN层的API

--- index.js
+++ index.js
@@ -3,4 +3,62 @@ import { NativeModules } from 'react-native';
 
 const { RNNativeRoute } = NativeModules;
 
-export default RNNativeRoute;
+const XCRoute = RNNativeRoute;
+
+function push(pageName, params) {
+    console.log('页面跳转：' + pageName);
+    return XCRoute.push(pageName, params);
+}
+
+function pop(pageName) {
+    XCRoute.pop(pageName);
+}
+
+/**
+ * 重置路由到首页
+ * @param tabName 首页的哪个tab
+ * @param pageName 回到首页后，立马哪个页面
+ * @param params
+ */
+function backTo(tabName, pageName = '', params = {}) {
+    XCRoute.backTo(tabName, pageName, params);
+}
+
+function replace(pageName, params) {
+    return XCRoute.replace(pageName, params);
+}
+
+function sendEvent(callbackId, params) {
+    XCRoute.sendEvent(callbackId, params);
+}
+
+function getRouters() {
+    return XCRoute.getRouters();
+}
+
+function setRightDrawPopEnabled(enable) {
+    return XCRoute.setRightDrawPopEnabled(enable);
+}
+
+export default {
+    push: throttle(push),
+    pop: throttle(pop),
+    backTo: backTo,
+    replace: throttle(replace),
+
+    sendEvent,
+    getRouters,
+    setRightDrawPopEnabled,
+};
+
+
+function throttle(fn) {
+    let lastTime = 0;
+    return function(pageName, params) {
+        const curTime = new Date().getTime();
+        if (curTime - lastTime >= 500) {
+            fn(pageName, params);
+            lastTime = curTime;
+        }
+    };
+}
